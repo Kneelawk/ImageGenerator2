38066ed207f17d847b4006a3110c5b204057bda0
Kneelawk <jedidiah.pommert@gmail.com>
2017-02-09 16:21:16 -0800

    Finish average and add Int rotate

--- src/main/scala/org/kneelawk/imagegenerator2/util/MathUtil.scala
+++ src/main/scala/org/kneelawk/imagegenerator2/util/MathUtil.scala
@@ -5,6 +5,19 @@ import scala.util.Random
 object MathUtil {
   val rand = new Random
   def rfloat(min: Float, max: Float) = rand.nextFloat() * (max - min) + min
+  
+  def rotate(value: Int, amount: Int, min: Int, max: Int): Int = {
+    val size = max - min
+    if (size > 0) {
+      var quot = (value.toDouble + amount.toDouble - min.toDouble) / size.toDouble
+      quot -= quot.floor
+      return (quot * size + min).toInt
+    } else if (size == 0) {
+      return min
+    } else {
+      throw new RuntimeException("Invalid rotate range")
+    }
+  }
 
   def rotate(value: Float, amount: Float, min: Float, max: Float): Float = {
     val size = max - min
@@ -47,7 +60,13 @@ object MathUtil {
   
   def average(a: Float, b: Float) = (a + b) / 2
   
-  def average(f: Float*) {
-    // TODO: finish average function
+  def average(a: Float, b: Float, c: Float) = (a + b + c) / 3
+  
+  def average(floats: Float*): Float = {
+    var v = 0f
+    for (f <- floats) {
+      v += f
+    }
+    return v / floats.length
   }
 }
